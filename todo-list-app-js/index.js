4a64963a31ce0c9dd55a8ec7167d062fc57ed7c3
mhamza-ali <hamza4841333@gmail.com>
2023-09-22 18:06:55 +0500

    display task items

--- todo-list-app-js/index.js
+++ todo-list-app-js/index.js
@@ -10,3 +10,23 @@ const displayCount = (taskCount) =>{
     countValue.innerHTML = taskCount;
 }
 
+const addTask = () => {
+    const taskName = newTaskInput.value.trim();
+    error.style.display = "none"
+    if(!taskName){
+        setTimeout(() => {
+            error.style.display = "block"
+        }, 200);
+        return;
+    }
+    const task = `<div class="task>
+    <input type="checkbox" class="task-check">
+    <span class="taskname">${taskName}</span>
+    <button clss="edit"><i class="fa-solid fa-pen-to-square"></i></button>
+    <button clss="delete"><i class="fa-solid fa-trash"></i></button>
+    </div>`;
+
+    tasksContainer.insertAdjacentHTML("beforeend", task);
+};
+
+addBtn.addEventListener("click", addTask);
