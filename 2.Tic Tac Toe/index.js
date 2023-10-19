6c48a981d2c9f803c65985f594fc95b7907b71c2
mhamza-ali <hamza4841333@gmail.com>
2023-10-20 00:15:39 +0500

    new project resume

--- 2.Tic Tac Toe/index.js
+++ 2.Tic Tac Toe/index.js
@@ -8,8 +8,17 @@ function createboard(){
     startcells.forEach((cell,index)=>{
         const cellelement = document.createElement('div')
         cellelement.classList.add('square')
+        cellelement.id = index
+        cellelement.addEventListener('click', addGo)
         gameboard.append(cellelement)
     })
 }
 
 createboard()
+
+function addGo(e){
+    const godisplay = document.createElement('div')
+    godisplay.classList.add('cross')
+    e.target.append(godisplay)
+
+}
