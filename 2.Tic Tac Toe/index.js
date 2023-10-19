1a9d9bd8d136c0c98fd93c0ac1a7552a619e45f6
mhamza-ali <hamza4841333@gmail.com>
2023-10-20 01:22:18 +0500

    done all but winning component not working

--- 2.Tic Tac Toe/index.js
+++ 2.Tic Tac Toe/index.js
@@ -4,6 +4,9 @@ const startcells = [
     "","","","","","","","",""
 ]
 
+let go = "circle"
+info.textContent = "Circle goes first"
+
 function createboard(){
     startcells.forEach((cell,index)=>{
         const cellelement = document.createElement('div')
@@ -18,9 +21,44 @@ createboard()
 
 function addGo(e){
     const godisplay = document.createElement('div')
-    godisplay.classList.add('cross')
+    godisplay.classList.add(go)
     e.target.append(godisplay)
-
+    go = go === "circle" ? "cross" : "circle"
+    info.textContent = "Its now " + go + "'s go."
+    e.target.removeEventListener("click", addGo)
+    checkScore()
 }
 
+function checkScore(){
+    const allsquares = document.querySelector(".square")
+    console.log(allsquares)
+    const winningCombos = [[0,1,2], [3,4,5], [6,7,8],
+    [0,3,6], [1,4,7], [2,5,8],
+    [0,4,8], [2,4,6]
+]
+
+winningCombos.forEach(array => {
+    const circleWins = array.every(cell => 
+        allsquares[cell].firstChild?.classList.contains('circle'))
+        if (circleWins) {
+            info.textContent = "Circle wins"
+            allsquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+
+        }
+})
+
+
+winningCombos.forEach(array => {
+    const crossWins = array.every(cell => 
+        allsquares[cell].firstChild?.classList.contains('cross'))
+        if (crossWins) {
+            info.textContent = "Cross wins"
+            allsquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+
+        }
+})
+
+}
 
